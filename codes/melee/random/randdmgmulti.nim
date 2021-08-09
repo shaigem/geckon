d35a1621ec893f9df81977ae8e3eace356610489
Ronnie Tran <ronnie.tran2@gmail.com>
2021-08-09 17:52:31 -0400

    Store the divisor and multiplier

--- codes/melee/random/randdmgmulti.nim
+++ codes/melee/random/randdmgmulti.nim
@@ -2,6 +2,7 @@ import geckon, ../melee
 
 const
     MaxMultiplier = 10.0
+    Divisor = 10
     OriginalCodeLine = ppc: lwz r0, 0x002C(sp)
 
 defineCodes:
@@ -9,31 +10,17 @@ defineCodes:
         authors "Ronnie"
         description "Every hit has a random multiplier applied to it"
         patchInsertAsm "80089284":
-            # free to use r0 and f31
-            
-            # Get random number in r3
+            # free to use: f31, r0, r30, r31
             {hsdRandi(max = (MaxMultiplier * 10).int, inclusive = true)}
-            # Cast to float and place it into f31
-            sth r3, 0x14(sp)
-            psq_l f31, 0x14(sp), 1, 5
+            # Cast the Divisor and our multiplier as floats into f31 
+            addis r3, r3, {Divisor}
+            stw r3, 0x14(sp)
+            psq_l f31, 0x14(sp), 0, 5
             # Multiply damage by multiplier
-            ps_mul f31, f1, f31 # f31 now has our new damage
-            # Load our divisor (10.0)
-            bl DivisorConstant
-            mflr r3
-            lfs f1, 0x0(r3) # f1 is free to use since f31 has our new damage
-            # Divide our new damage by 10
-            ps_div f31, f31, f1
-            # Store result in f1 to ensure 64-bit double
-            fmr f1, f31
+            ps_muls1 f1, f1, f31
+            # Divide our new damage by 10 and store in f1
+            ps_div f1, f1, f31
             b OriginalExit
             
-            DivisorConstant:
-                blrl
-                ".float"10.0
-
             OriginalExit:            
                 {OriginalCodeLine}
-
-
-        
