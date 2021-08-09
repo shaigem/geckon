8013ce10c09aa86632931867ee68209040cb1e4e
Ronnie Tran <ronnie.tran2@gmail.com>
2021-08-09 17:52:06 -0400

    Account for meteor and 362 angle

--- codes/melee/random/randangles.nim
+++ codes/melee/random/randangles.nim
@@ -1,20 +1,44 @@
 import geckon, ../melee
 
 const 
-    OriginalCodeLine = ppc: stw r0, 0x0004 (r31)
     MaxAngle = 362
-    RegisterRandomAngle = $Register.r3
-    RegisterAngleResult = $Register.r0
 
 defineCodes:
     createCode "Random Knockback Angles":
         authors "Ronnie"
         description "Every hit has a random angle"
-        patchInsertAsm "8007a934":
+
+        # Random angle for Projectiles
+        patchInsertAsm "802792e4":
+            # Backup r0, r4 and r5
+            stwu r1, -16(r1)
+            stw r4, 0x4(r1)
+            stw r0, 0x8(r1)
+            stw r5, 0xC(r1)
             # Get our random angle (from 0 to MaxAngle) into r3
             {hsdRandi(max = MaxAngle, inclusive = true)}
-            # Set r0 = r3
-            mr {RegisterAngleResult}, {RegisterRandomAngle}
+            stw r3, 0x0020(r29) # original code line
+            # Restore r0, r4 and r5
+            lwz r5, 0xC(r1)
+            lwz r0, 0x8(r1)
+            lwz r4, 0x4(r1)
+            addi r1, r1, 0x10
+            
+        # Random angle for Normal Hitboxes
+        patchInsertAsm "8007aca0":
+            # backup r0, r3 and r5
+            stwu r1, -16(r1)
+            stw r3, 0x4(r1)
+            stw r0, 0x8(r1)
+            stw r5, 0xC(r1)
+            # Get our random angle (from 0 to MaxAngle) into r3
+            {hsdRandi(max = MaxAngle, inclusive = true)}
+            # Set r4 = r3
+            mr r4, r3
+            # restore r0, r3 and r5
+            lwz r5, 0xC(r1)
+            lwz r0, 0x8(r1)
+            lwz r3, 0x4(r1)
+            addi r1, r1, 0x10
             # Call the original code line and exit
-            {OriginalCodeLine}
-
+            cmplwi r4, 361
