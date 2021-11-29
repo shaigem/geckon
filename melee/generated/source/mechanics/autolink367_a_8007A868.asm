5aae5bc7506fbe33503a429aae86c1b91978c4cc
Ronnie Tran <ronnie.tran2@gmail.com>
2021-11-28 23:20:16 -0500

    first version of autolink release

--- melee/generated/source/mechanics/autolink367_a_8007A868.asm
+++ melee/generated/source/mechanics/autolink367_a_8007A868.asm
@@ -1,42 +1,48 @@
 lwz r0, 0x00000020(r3)
 cmplwi r0, 367
 bne OriginalExit
-bl Data
-mflr r5
-lwz r4, 0x000000E0(r25)
-cmpwi r4, 0
-bne CalculateVelHitbox
-lfs f0, 0x00000010(r5)
-fmr f25, f0
-b OriginalExit
-CalculateVelHitbox:
 mr r6, r3
-lfs f1, 0(r5)
-lfs f2, 0x00000004(r5)
-addi r4, r3, 0x0000004C
+bl Constants
+mflr r3
+lfs f1, 0(r3)
+lfs f2, 0x00000004(r3)
+addi r4, r6, 0x0000004C
 lwz r3, 0(r25)
-bl ToPointFunc
+bl MoveTowardsPoint
+lfs f0, 0xFFFF8900(rtoc)
 lfs f5, 0x0000004C(r6)
 lfs f6, 0x000000B0(r25)
 fsubs f5, f6, f5
-lfs f0, 0xFFFF8900(rtoc)
 fcmpo cr0, f5, f0
-bge+ CheckReverseForAttackerDefender
+bge+ CheckReverseForAtkDef
 fneg f2, f2
-CheckReverseForAttackerDefender:
+CheckReverseForAtkDef:
 lfs f3, 0x000000CC(r15)
 lfs f4, 0x000000C8(r15)
 lfs f5, 0x000000B0(r15)
 lfs f6, 0x000000B0(r25)
 fsubs f5, f6, f5
-lfs f0, 0xFFFF8900(rtoc)
 fcmpo cr0, f5, f0
-bge+ NotReverse
-lfs f0, 0xFFFF8910(rtoc)
-fmuls f4, f0, f4
-NotReverse:
+bge+ CalculateDirAndKb
+fneg f4, f4
+CalculateDirAndKb:
 fadds f1, f3, f1
 fadds f2, f4, f2
+CalcKb:
+stwu sp, 0xFFFFFFE0(sp)
+addi r3, sp, 0x00000014
+stfs f0, 0x0000001C(sp)
+stfs f1, 0x00000018(sp)
+stfs f2, 0x00000014(sp)
+lis r12, 0x80342dfc @h
+ori r12, r12, 0x80342dfc @l
+mtctr r12
+bctrl
+fadds f26, f26, f1
+lfs f1, 0x00000018(sp)
+lfs f2, 0x00000014(sp)
+addi sp, sp, 0x00000020
+CalcDir:
 lis r12, 0x80022c30 @h
 ori r12, r12, 0x80022c30 @l
 mtctr r12
@@ -48,15 +54,19 @@ stfs f24, 0(r31)
 stfd f0, 0x00000210(sp)
 lwz r3, 0x00000214(sp)
 cmpwi r3, 0
-bge Exit
+bge Epilog
 addi r3, r3, 360
-Exit:
+Epilog:
 stw r3, 0x00000004(r31)
 lis r12, 0x8007a938 @h
 ori r12, r12, 0x8007a938 @l
 mtctr r12
 bctr
-ToPointFunc:
+Constants:
+blrl
+.float 0.16
+.float 0.08
+MoveTowardsPoint:
 mflr r0
 stw r0, 4(r1)
 stwu r1, 0xFFFFFFA8(r1)
@@ -75,47 +85,33 @@ lis r12, 0x8000D4F8 @h
 ori r12, r12, 0x8000D4F8 @l
 mtctr r12
 bctrl
-lfs f1, 0x0000002C(r1)
-lfs f0, 0x00000030(r1)
-fmuls f2, f1, f1
-lfs f3, 0x00000034(r1)
-fmuls f1, f0, f0
-lfs f0, 0xFFFFA818(rtoc)
-fmuls f3, f3, f3
-fadds f1, f2, f1
-fadds f29, f3, f1
-fcmpo cr0, f29, f0
-ble lbl_8015BEF8
-frsqrte f1, f29
-lfd f3, 0xFFFFA828(rtoc)
-lfd f2, 0xFFFFA830(rtoc)
-fmul f0, f1, f1
-fmul f1, f3, f1
-fnmsub f0, f29, f0, f2
-fmul f1, f1, f0
-fmul f0, f1, f1
-fmul f1, f3, f1
-fnmsub f0, f29, f0, f2
-fmul f1, f1, f0
-fmul f0, f1, f1
-fmul f1, f3, f1
-fnmsub f0, f29, f0, f2
-fmul f0, f1, f0
-fmul f0, f29, f0
-frsp f0, f0
-stfs f0, 0x00000024(r1)
-lfs f29, 0x00000024(r1)
+addi r3, sp, 0x0000002C
+lis r12, 0x80342dfc @h
+ori r12, r12, 0x80342dfc @l
+mtctr r12
+bctrl
+fmr f29, f1
 lbl_8015BEF8:
 fcmpo cr0, f29, f30
 bge lbl_8015BF0C
-b lbl_8015BF40
+b MoveTowardsPointEpilog
 lbl_8015BF0C:
 addi r3, r1, 0x0000002C
 lis r12, 0x8000D2EC @h
 ori r12, r12, 0x8000D2EC @l
 mtctr r12
 bctrl
-lbl_8015BF40:
+fmuls f1, f29, f31
+lfs f0, 0x0000002C(r1)
+fmuls f0, f0, f1
+stfs f0, 0x0000002C(r1)
+lfs f0, 0x00000030(r1)
+fmuls f0, f0, f1
+stfs f0, 0x00000030(r1)
+lfs f0, 0x00000034(r1)
+fmuls f0, f0, f1
+stfs f0, 0x00000034(r1)
+MoveTowardsPointEpilog:
 lfs f2, 0x0000002C(r1)
 lfs f1, 0x00000030(r1)
 lwz r0, 0x0000005C(r1)
@@ -127,12 +123,5 @@ lwz r30, 0x00000038(r1)
 addi r1, r1, 0x00000058
 mtlr r0
 blr
-Data:
-blrl
-.float 0.16
-.float 0.08
-.float 0.5
-.float 10.0
-.float 80.0
 OriginalExit:
 lwz r0, 0x00000020(r3)
--- melee/source/mechanics/autolink367.nim
+++ melee/source/mechanics/autolink367.nim
@@ -1,92 +1,110 @@
 import geckon
 
+const 
+    Angle = 367
+    Steps = 0.08
+    MinStepDistance = 0.16
+
+const 
+    PSVecMagFunc = "0x80342dfc"
+    SubtractVector3DFunc = "0x8000D4F8"
+    Atan2Func = "0x80022c30"
 
 defineCodes:
-    createCode "Autolink 367 Degrees":
-        description ""
-        authors "Ronnie/sushie"
+    createCode "Autolink 367":
+        description "Special angle that tries to keep victims in"
+        authors "sushie", "rmn"
         
-#[         patchInsertAsm "8008e73c":
-            # r31 = fighter data
-            # r30 = fighter gobj
+        # autolink 367
+        patchInsertAsm "8007a868":
+            # 1. defender is moved towards hitbox pos
+            # 2. defender is moved in the direction the attacker is moving to
+            # 3. knockback is adjusted based on distance between hitbox pos and defender
+            # r3 = hit struct
+            # r15 = attacker data
+            # r25 = defender data
+            # r17 = damage source?
 
-            %load("0x80459278", r3)
-            lwz r5, 0x8(r3) # gobj of source
-            lwz r4, 0x1868(r31) # dmg source of fighter
-            cmplw r4, r5
-            bne OriginalExit
-            lwz r4, 0xC(r3) 
-            lwz r4, 0x20(r4) # angle of last hit
-            cmplwi r4, 367
-            bne OriginalExit
+            # first check hitbox angle
+            lwz r0, 0x20(r3) # angle of hit struct
+            cmplwi r0, {Angle}
+            bne OriginalExit # if not autolink angle, exit
 
+            # TODO should grounded defenders take a set angle of 80?
+            mr r6, r3 # backup hit struct to r6
+            bl Constants
+            mflr r3
+            lfs f1, 0(r3)
+            lfs f2, 0x4(r3)
+            addi r4, r6, 0x4C # point to pos of hitbox
+            lwz r3, 0(r25) # defender gobj
+            bl MoveTowardsPoint
+            # now f1 = next step y
+            # f2 = next step x
+            # check for reverse hits
+            lfs f0, -0x7700(rtoc) # 0.0
+            lfs f5, 0x4C(r6) # hitbox x
+            lfs f6, 0xB0(r25) # Defender x
+            fsubs f5, f6, f5
+            fcmpo cr0, f5, f0
+            %`bge+`(CheckReverseForAtkDef)
+            fneg f2, f2 # negate the next step x
+            CheckReverseForAtkDef:
+                    lfs f3, 0xCC(r15) # attacker y vel
+                    lfs f4, 0xC8(r15) # attacker x vel
+                    lfs f5, 0xB0(r15) # attacker x
+                    lfs f6, 0xB0(r25) # defender x
+                    fsubs f5, f6, f5 # def x - atk x
+                    fcmpo cr0, f5, f0
+                    %`bge+`(CalculateDirAndKb)
+                    fneg f4, f4 # negate attacker x vel
+            CalculateDirAndKb:
+                fadds f1, f3, f1 # attacker y vel + next step y
+                fadds f2, f4, f2 # attacker x vel + next step x
+                CalcKb:
+                    stwu sp, -0x20(sp)
+                    addi r3, sp, 0x14
+                    stfs f0, 0x1C(sp) # z
+                    stfs f1, 0x18(sp) # y
+                    stfs f2, 0x14(sp) # x
+                    %branchLink(PSVecMagFunc) # PSVecMag
+                    fadds f26, f26, f1 # add old kb value with our new one
+                    # restore f1 & f2
+                    lfs f1, 0x18(sp) # y
+                    lfs f2, 0x14(sp) # x
+                    addi sp, sp, 0x20
+                CalcDir:
+                    %branchLink(Atan2Func) # atan2
+                    lfs f0, -0x76C4(rtoc) # 180/PI convert radian to degrees
+                    fmuls f25, f0, f1
+                    fctiwz f0, f25
+                    stfs f24, 0(r31)
+                    stfd f0, 0x210(sp)
+                    # make angle positive for meteor canceling
+                    lwz r3, 0x214(sp)
+                    cmpwi r3, 0
+                    bge Epilog # angle is positive
+                    addi r3, r3, 360 # add 360 to get the postivie equivalent
 
-            # TODO anymore checks?
+            Epilog:
+                stw r3, 0x4(r31) # store our new angle
+                %branch("0x8007a938")
 
-            lwz r4, 0x1868(r31) # gobj of attacker
-            lwz r4, 0x2C(r4) # data of attacker
+            Constants:
+                blrl
+                %`.float`(MinStepDistance) # minimum end step
+                %`.float`(Steps) # step (was 0.08)
 
-
-
-            bl Data
-            mflr r7
-            lfs f1, 4(r7)
-            lfs f2, 8(r7)
-            lwz r4, 0xC(r3)
-            addi r4, r4, 0x4C
-            mr r3, r30
-            bl ToPointFunc
-            lfs f0, 0(r7) # 0.20
-            fmuls f3, f2, f0 # 20% of diff x
-            fmuls f4, f1, f0 # 20% of diff y
-            lfs f5, 0x8C(r31)
-            fadds f3, f5, f3
-            lfs f5, 0x90(r31)
-            fadds f4, f5, f4
-
-            stfs f3, 0x8C(r31)
-            stfs f4, 0x90(r31) 
-            AdjustSpeed:
-                
-#[                 lwz r5, 0xC(r3) # get hit
-                lfs f3, 0xB0(r31) # our x pos
-                lfs f4, 0xB4(r31) # our y pos
-                lfs f5, 0x4C(r5) # hit x pos
-                lfs f6, 0x50(r5) # hit y pos
-                fsubs f3, f3, f5 # diff x
-                fsubs f4, f4, f6 # diff y
-
-                bl Data
-                mflr r3
-                lfs f0, 0(r3) # 0.20
-                fmuls f3, f3, f0 # 20% of diff x
-                fmuls f4, f4, f0 # 20% of diff y
-
-                lfs f5, 0x80(r4)
-                lfs f6, 0x84(r4)
-                fsubs f3, f5, f3
-                fsubs f4, f6, f4
-                
-                lfs f5, 0x8c(r31)
-                lfs f6, 0x90(r31)
-
-                fadds f3, f3, f5
-                fadds f4, f4, f6
-
-                stfs f3, 0x8C(r31)
-                stfs f4, 0x90(r31) ]#
-                b OriginalExit
-
-#                bl Data
-#                mflr r3
-#                lfs f4, 0x8(r3)
-#                fmuls f26, f4, f3 # 0.5 * velocity
-
-#[ f1 = speed stuff
-   f2 = speed stuff
-   r3 = our gobj
-   r4 = vec pos of where to go ]#
-            ToPointFunc:
+            MoveTowardsPoint:
+                # linear interop
+                # inputs
+                # f1 = minimum end step
+                # f2 = steps
+                # r3 = fighter GOBJ (start)
+                # r4 = end pos ptr
+                # outputs
+                # f1 = y
+                # f2 = x
                 mflr r0
                 stw r0, 4(r1)
                 stwu r1, -0x58(r1)
@@ -97,67 +115,34 @@ defineCodes:
                 stfd f29, 0x40(r1)
                 stw r31, 0x3c(r1)
                 stw r30, 0x38(r1)
-#                addi r30, r5, 0
                 addi r5, r1, 0x2c
                 lwz r31, 0x2c(r3)
                 addi r3, r4, 0
-                addi r4, r31, 0xb0
-                %branchLink("0x8000D4F8")
-                #bl func_8000D4F8
-                lfs f1, 0x2c(r1)
-                lfs f0, 0x30(r1)
-                fmuls f2, f1, f1
-                lfs f3, 0x34(r1)
-                fmuls f1, f0, f0
-                lfs f0, -0x57E8(rtoc)
-                fmuls f3, f3, f3
-                fadds f1, f2, f1
-                fadds f29, f3, f1
-                fcmpo cr0, f29, f0
-                ble lbl_8015BEF8
-                frsqrte f1, f29
-                lfd f3, -0x57D8(rtoc)
-                lfd f2, -0x57D0(rtoc)
-                fmul f0, f1, f1
-                fmul f1, f3, f1
-                fnmsub f0, f29, f0, f2
-                fmul f1, f1, f0
-                fmul f0, f1, f1
-                fmul f1, f3, f1
-                fnmsub f0, f29, f0, f2
-                fmul f1, f1, f0
-                fmul f0, f1, f1
-                fmul f1, f3, f1
-                fnmsub f0, f29, f0, f2
-                fmul f0, f1, f0
-                fmul f0, f29, f0
-                frsp f0, f0
-                stfs f0, 0x24(r1)
-                lfs f29, 0x24(r1)
+                addi r4, r31, 0xB0
+                %branchLink(SubtractVector3DFunc)
+                addi r3, sp, 0x2C
+                %branchLink(PSVecMagFunc)
+                fmr f29, f1
                 lbl_8015BEF8:
                     fcmpo cr0, f29, f30
                     bge lbl_8015BF0C
-                    b lbl_8015BF40
+                    b MoveTowardsPointEpilog
                 lbl_8015BF0C:
-#                    stfs f29, 0(r30)
                     addi r3, r1, 0x2c
                     %branchLink("0x8000D2EC")
-#                    bl func_8000D2EC
-#                    fmuls f1, f29, f31
-#                    lfs f0, 0x2c(r1)
-#                    fmuls f0, f0, f1
-#                    stfs f0, 0x2c(r1)
-#                    lfs f0, 0x30(r1)
-#                    fmuls f0, f0, f1
-#                    stfs f0, 0x30(r1)
-#                    lfs f0, 0x34(r1)
-#                    fmuls f0, f0, f1
-#                    stfs f0, 0x34(r1)
-                lbl_8015BF40:
+                    fmuls f1, f29, f31
+                    lfs f0, 0x2c(r1)
+                    fmuls f0, f0, f1
+                    stfs f0, 0x2c(r1)
+                    lfs f0, 0x30(r1)
+                    fmuls f0, f0, f1
+                    stfs f0, 0x30(r1)
+                    lfs f0, 0x34(r1)
+                    fmuls f0, f0, f1
+                    stfs f0, 0x34(r1)
+                MoveTowardsPointEpilog:
                     lfs f2, 0x2c(r1)
-#                    stfs f0, 0x8c(r31)
                     lfs f1, 0x30(r1)
-#                    stfs f0, 0x90(r31)
                     lwz r0, 0x5c(r1)
                     lfd f31, 0x50(r1)
                     lfd f30, 0x48(r1)
@@ -168,18 +153,10 @@ defineCodes:
                     mtlr r0
                     blr
 
-
-            Data:
-                blrl
-                %`.float`(0.20)
-                %`.float`(0.16)
-                %`.float`(0.08)
-
             OriginalExit:
-                lwz r3, -0x514C(r13) # restore r3
-                lfs f2, 0x0620(r31)
+                lwz r0, 0x20(r3)
 
- ]#
+#[ 
         patchInsertAsm "8007a868":
             # r3 = Hit struct
             # r15 = attacker data
@@ -242,6 +219,40 @@ defineCodes:
                 NotReverse:
                     fadds f1, f3, f1
                     fadds f2, f4, f2
+
+                    fmr f3, f2
+                    fmr f4, f1
+                    fmuls f3, f3, f3 # vel x squared
+                    fmuls f4, f4, f4 # vel y squared
+                    fadds f3, f4, f3 # add velocities together
+                    frsqrte f4, f3 # sqrt
+                    # f4 = recipricol = f1
+                    # f29 = f3
+                    # f3 = f6
+                    # f2 = f5
+
+                    lfd f6, -0x57D8(rtoc)
+                    lfd f5, -0x57D0(rtoc)
+                    fmul f0, f4, f4
+                    fmul f4, f6, f4
+                    fnmsub f0, f3, f0, f5
+
+                    fmul f4, f4, f0
+                    fmul f0, f4, f4
+                    fmul f4, f6, f4
+                    fnmsub f0, f3, f0, f5
+
+                    fmul f4, f4, f0
+                    fmul f0, f4, f4
+                    fmul f4, f6, f4
+                    fnmsub f0, f3, f0, f5
+
+                    fmul f0, f4, f0
+                    fmul f0, f3, f0
+                    frsp f0, f0
+                    fmr f3, f0
+                    fadds f26, f26, f3 # 0.5 * velocity
+
 #                    stfs f1, 0x90(r25)
 #                    stfs f2, 0x8C(r25)
 #            %branchLink("0x80022c30") # atan2
@@ -328,16 +339,16 @@ defineCodes:
                     addi r3, r1, 0x2c
                     %branchLink("0x8000D2EC")
 #                    bl func_8000D2EC
-#                    fmuls f1, f29, f31
-#                    lfs f0, 0x2c(r1)
-#                    fmuls f0, f0, f1
-#                    stfs f0, 0x2c(r1)
-#                    lfs f0, 0x30(r1)
-#                    fmuls f0, f0, f1
-#                    stfs f0, 0x30(r1)
-#                    lfs f0, 0x34(r1)
-#                    fmuls f0, f0, f1
-#                    stfs f0, 0x34(r1)
+                    fmuls f1, f29, f31
+                    lfs f0, 0x2c(r1)
+                    fmuls f0, f0, f1
+                    stfs f0, 0x2c(r1)
+                    lfs f0, 0x30(r1)
+                    fmuls f0, f0, f1
+                    stfs f0, 0x30(r1)
+                    lfs f0, 0x34(r1)
+                    fmuls f0, f0, f1
+                    stfs f0, 0x34(r1)
                 lbl_8015BF40:
                     lfs f2, 0x2c(r1)
 #                    stfs f0, 0x8c(r31)
@@ -355,10 +366,10 @@ defineCodes:
             Data:
                 blrl
                 %`.float`(0.16)
-                %`.float`(0.08)
+                %`.float`(0.20) # was 0.08
                 %`.float`(0.50)
                 %`.float`(10)
-                %`.float`(80)
+                %`.float`(80) ]#
 
             # two vectors
             # first = attacker's direction vector
@@ -571,8 +582,8 @@ defineCodes:
  #           %branch("0x8007a8f0", r0)
 #            %restore
 
-            OriginalExit:
-                lwz r0, 0x20(r3)
+#            OriginalExit:
+#                lwz r0, 0x20(r3)
 
 # defineCodes:
 #     createCode "Autolink 367 Degrees":
